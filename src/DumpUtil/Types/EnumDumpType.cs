b50d8c4617f4f90e4d2995a6aa68115df4b11262
cmc19 <carisch19+git@gmail.com>
2016-12-22 10:54:28 -0500

    Fix AIO

--- src/DumpUtil/Types/EnumDumpType.cs
+++ src/DumpUtil/Types/EnumDumpType.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DumpUtil
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
     abstract class BaseEnumDumpType:DumpType
     {
         
